7565d60533706cd6f5b9846756f3cab01452f73c
glenn <glenn.jo@kakaocorp.com>
2016-10-10 15:03:13 +0900

    implicit 2

--- src/test/scala/ImplicitSpec.scala
+++ src/test/scala/ImplicitSpec.scala
@@ -1,5 +1,7 @@
 import org.scalatest.{Matchers, FunSuite}
 
+import scala.concurrent.Future
+
 /**
  * Created by glenn on 2016. 9. 28..
  */
@@ -69,9 +71,82 @@ class ImplicitSpec extends FunSuite with Matchers{
 
     println(a4.barbarbar)
     println(new Foo(a4).barbarbar)
-    //
-  }
 
+
+    //implicit part 2
+    //class의 생성자 --> 안됨
+    /*case class Bar(a: String)
+    case class Cheese(c: Int, b: Bar)
+
+    implicit class Foo2(c: Cheese)(implicit bar : Bar){
+      def barbar(): String ={
+        c.copy(b = bar).b.a
+      }
+    }
+
+    val cheese = Cheese(10, Bar("cheese"))
+    cheese.barbar()*/
+
+
+
+
+    test("실전"){
+      //1.암묵적인 type casting
+
+      //Edina(sul: String) ==> Suldina(edina: String)
+      case class Edina(sul: String)
+      case class Suldina(edina: String)
+
+
+      /*
+      case class Status(status: Int)
+      //implicit def foo2(bar: Int): String = bar.toString + " 10000"
+      implicit def foo2(bar: Int): Status = Status(bar)
+      */
+
+      //implicit class TypeCasting(sul: String): Suldina = Suldina(sul)
+      //implicit class TypeCasting(sul: String)
+      implicit def casting(edina: Edina): Suldina = Suldina(edina.sul)
+      val suldina : Suldina = Edina("isool")
+      println(suldina)
+
+      //2.기존에 있는 class 에 문법 추가하기
+      //Edina에 name이라는 함수를 추가해봅시다.
+
+      //시도
+      //implicit class EdinaName(sul: String, name: String): Edina
+
+      //클래스의 생성자가 중요
+      //정답
+      implicit class EdinaName(e: Edina){
+        def name = "에디나"
+      }
+
+      val e1 = Edina("막걸리")
+      e1.name == "에디나"
+
+
+      //3. 암무적인 변수가 있음
+      //암묵적인 변수를 특정 클래스에 주입
+      //val ec = scala.concurrent.ExecutionContext.Implicits.global
+      implicit val ec = scala.concurrent.ExecutionContext.Implicits.global
+      /*Future{
+        println(10)
+        Thread.sleep(1000)
+        1+1
+      }(ec)*/
+      val a = Future[Int](2)(ec)
+      val list = List[Int](2)
+      val future = Future[Int](2)
+      val set = Set[Int](2)
+
+
+    }
+
+
+
+
+  }
 }
 
 
---
+++ src/test/scala/TraitSpec.scala
@@ -0,0 +1,6 @@
+/**
+ * Created by glenn on 2016. 10. 10..
+ */
+class TraitSpec {
+
+}
